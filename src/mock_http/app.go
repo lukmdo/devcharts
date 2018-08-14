84673d473e495f4e78611e3a5977f7ac9f1693bd
Lukasz Dobrzanski <lukasz.m.dobrzanski@gmail.com>
2018-08-14 20:51:08 +0100

    add go buildTags

--- src/mock_http/app.go
+++ src/mock_http/app.go
@@ -4,11 +4,37 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"flag"
+	"os"
+)
+
+var (
+	Version = "?"
+	VersionUTC = "?"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "ok")
+	fmt.Fprintf(w, "ok: %s", Version)
 }
+
+func init() {
+	argHelp := flag.Bool("help", false, "Show this help")
+	argVersion := flag.Bool("version", false, "Show version")
+	flag.Parse()
+
+	if *argHelp {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	if *argVersion {
+		fmt.Printf("Version: %s\n", Version)
+		fmt.Printf("UTC Build Time: %s\n", VersionUTC)
+		os.Exit(0)
+
+	}
+}
+
 
 func main() {
 	http.Handle("/", loggerMW(http.HandlerFunc(handler)))
